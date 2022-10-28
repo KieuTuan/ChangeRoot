5f5df9e7233e18f431f95550cd66c761c41b7e48
Kieu Anh Tuan <kieutuan25998@gmail.com>
2022-10-28 10:39:48 +0700

    add loading button

--- ChangeRoot/Module/Home/HomeViewController.swift
+++ ChangeRoot/Module/Home/HomeViewController.swift
@@ -8,9 +8,14 @@
 import UIKit
 
 final class HomeViewController: BaseViewController {
+    @IBOutlet private weak var loadingButton: LoadingButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Home"
+        loadingButton.showLoading()
+        DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+            self.loadingButton.hideLoading()
+        }
     }
 }
